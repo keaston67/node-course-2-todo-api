0c24e0ee74c852eebbe71242077eb4f5d0bea6ff
Kim Easton <keaston@Kims-MacBook-Pro.local>
2018-08-30 17:26:48 +1200

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 // set mongoose promise library 
 mongoose.Promise = global.Promise;
 // mongoose connection to database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 // export mongoose so variable is available / returned when this file is required
 module.exports = {mongoose};
